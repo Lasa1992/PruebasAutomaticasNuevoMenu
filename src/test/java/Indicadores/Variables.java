eef87222518fa299032709ff3257591a1a5c6796
David Montoya <david.calidadgm@gmail.com>
2025-04-08 17:20:06 -0700

    Se agregó la clase para validar los pagos con factoraje.

--- src/test/java/Indicadores/Variables.java
+++ src/test/java/Indicadores/Variables.java
@@ -1,14 +1,15 @@
 package Indicadores;
 
 public class Variables {
-    public static final String CLIENTE = "000001"; // Número de cliente configurable
-    public static final String RUTA = "000004";      // Folio de la ruta configurable
-    public static final String DocumentoIngreso = "CARTA PORTE CFDI - INGRESO"; // Número de documento de traslado
-    public static final String DocumentoTraslado = "CARTA PORTE CFDI - TRASLADO"; // Número de documento de traslado
-    public static final String Operador = "024518"; // Número de operador/*024518*/
+    public static final String CLIENTE = "000005"; // Número de cliente configurable
+    public static final String RUTA = "000025";      // Folio de la ruta configurable
+    public static final String DocumentoIngreso = "CARTA PORTE CFDI - CCP"; // Número de documento de traslado
+    public static final String DocumentoTraslado = "CARTA PORTE CFDI - CCPT"; // Número de documento de traslado
+    public static final String Operador = "000001"; // Número de operador/*024518*/
     public static final String PROVEEDOR = "1"; // Número de proveedoR
 
     // Variables importacion archivos.
 
     public static final String Docmateriales = "C:\\RepositorioPrueAuto\\XLSXPruebas\\ImportarMaterialesPA.xlsx";
+    public static final String XMLFactoraje = "C:\\RepositorioPrueAuto\\XLSXPruebas\\Factoraje 27272.xml";
 }
